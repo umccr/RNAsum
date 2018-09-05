4d62750bb7f5c8b85fd3b63df3bcff179509a316
Sehrish Kanwal <kanwals@unimelb.edu.au>
2018-09-05 10:17:28 +1000

    joint fusion calls

--- fusions/fusion-comparison.R
+++ fusions/fusion-comparison.R
@@ -1,11 +1,24 @@
 library("dplyr")
 
-#Read in the input files
+#read in the input files
 oncofuse <- read.table(file = '~/Documents/UMCCR/data/fusions/comparison/MH17T001P013-oncofuse-edited', header = TRUE)
 pizzly <- read.table(file = '~/Documents/UMCCR/data/fusions/comparison/MH17T001P013-oncofuse-test-flat-filtered.tsv', header = TRUE)
 
 #sort oncofuse output on driver probability of fusions AND filter the sorted oncofuse file to extract
 #rows with driver probability above a certain threshhold (currently using 0.6)
-oncofuse.sorted <- filter(arrange(oncofuse, desc(oncofuse$DRIVER_PROB)), DRIVER_PROB >= 0.6)
+oncofuse.sorted.filtered <- filter(arrange(oncofuse, desc(oncofuse$DRIVER_PROB)), DRIVER_PROB >= 0.6)
+
+#sort pizzly output on paircount for the fusion genes AND filter the sorted pizzly file to extract
+#rows with paircount values above a certain threshhold (currently using 5)
+pizzly.sorted.filtered <- filter(arrange(pizzly, desc(pizzly$paircount)), paircount >= 5)
+
+#applying semi_join on oncofuse.sorted.filter and pizzly.sorted.filtered
+joint.fusion.calls <- semi_join(oncofuse.sorted.filtered, pizzly.sorted.filtered,
+                                by = c("X5_FPG_GENE_NAME" = "geneA.name", "X3_FPG_GENE_NAME" = "geneB.name"))
+
+#in case pizzly pizzly gene naming is not according to 5 -> 3 convention. Couldn't find a clear answer to this online
+joint.fusion.calls.2 <- semi_join(oncofuse.sorted.filtered, pizzly.sorted.filtered,
+                                by = c("X5_FPG_GENE_NAME" = "geneB.name", "X3_FPG_GENE_NAME" = "geneA.name"))
+
 
 
