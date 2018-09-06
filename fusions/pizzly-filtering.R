e177b0094b79b78db3c3ea2e2d2468342f876441
Sehrish Kanwal <kanwals@unimelb.edu.au>
2018-09-06 16:06:39 +1000

    extracting fusion genes that are in filtered quantification file

--- fusions/pizzly-filtering.R
+++ fusions/pizzly-filtering.R
@@ -10,10 +10,23 @@ quant <- read.table(file = '~/Documents/UMCCR/data/fusions/pizzly-validation/abu
 quant.sorted.filtered <- filter(arrange(quant, desc(quant$tpm)), tpm >= (mean(quant$tpm)))
 
 #pizzly.fusions$transcripts.list is a factor. Coerce the argument to character first to be able to use sapply
-pizzly.fusions$transcripts.list <- as.character(pizzly.fusions$transcripts.list)
+#pizzly.fusions$transcripts.list <- as.character(pizzly.fusions$transcripts.list)
 
-
-quant.transcripts <- sapply(pizzly.fusions$transcripts.list, function(x){
-  y <- strsplit(x, ";")
+data <- data.frame()
+df <- apply(pizzly.fusions[,c('geneA.name','geneB.name','transcripts.list')],1, function(x){
+  y <- strsplit(x['transcripts.list'], ";")
+  y <- unname(y)
+  for (i in 1:length(y[[1]])){
+    #print(length(y[[1]]))
+    #print(y[[1]][i])
+    if (y[[1]][i] %in% quant.sorted.filtered$target_id){
+      #print("yes")
+      #print(x['geneA.name'])
+      df <- data.frame(geneA = x['geneA.name'], geneB = x['geneB.name'], stringsAsFactors=FALSE)
+      data <- rbind(data, df)
+    }
+  }
+  print(data)
 })
-quant.transcripts.mod <- unname(quant.transcripts)
+
+
