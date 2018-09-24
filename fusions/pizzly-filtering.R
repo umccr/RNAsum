76417d7e0d535f5d9708392d79c4c582e7f54eb0
Sehrish Kanwal <kanwals@unimelb.edu.au>
2018-09-24 14:01:27 +1000

    filtering data on the basis of upper third quartile value for tpm

--- fusions/pizzly-filtering.R
+++ fusions/pizzly-filtering.R
@@ -7,9 +7,10 @@ pizzly.fusions <- read.table(file = '~/Documents/UMCCR/data/fusions/pizzly-valid
 #read in the transcripts quantification file
 quant <- read.table(file = '~/Documents/UMCCR/data/fusions/pizzly-validation/abundance.tsv', header = TRUE)
 
-#sort and filter quantification file on tpm values. Currently filtering on transcripts that have higher value than
-#the mean transcript value.
-quant.sorted.filtered <- filter(arrange(quant, desc(quant$tpm)), tpm >= (mean(quant$tpm)))
+#sort and filter quantification file on tpm values. Currently filtering on quantiles. Selected 0.99 because that reduces the final fusion
+#calls to the value we are interested in (~15)
+
+quant.sorted.filtered <- filter(arrange(quant, desc(quant$tpm)), tpm >= (quantile(quant$tpm, 0.997)))
 
 #pizzly.fusions$transcripts.list is a factor. Coerce the argument to character first to be able to use sapply
 #pizzly.fusions$transcripts.list <- as.character(pizzly.fusions$transcripts.list)
@@ -17,7 +18,29 @@ quant.sorted.filtered <- filter(arrange(quant, desc(quant$tpm)), tpm >= (mean(qu
 #initialize an empty dataframe
 result <- data.frame()
 result.list <- vector("list", length = nrow(pizzly.fusions))
-#inter <- 1
+
+#let's try using for loop for iterating over pizzly.fusions dataframe and get transcriptID and fusion gene pair information.
+#can also filter quant.sorted.filtered$target_id to have only fusion gene target ids (that is two tracscripts instead of one-
+#this will increase speed
+
+for (row in 1:nrow(pizzly.fusions)){
+  y <- strsplit(as.character(pizzly.fusions[row, 7]), "\\;")
+  y <- unname(y)
+  for (i in 1:length(y[[1]])){
+    if (y[[1]][i] %in% quant.sorted.filtered$target_id){
+      #val <- c(y[[1]][i], as.character(pizzly.fusions[row,1]), as.character(pizzly.fusions[row, 3]))
+      #creating a new dataframe for the filtered pizzly results
+      result <- rbind(result, data.frame(pizzly.fusions[row,]))
+      #result.list[[row]] <- val
+    }
+  }
+}
+
+#remove nulls from result list
+#result.list <- result.list[lapply(result.list, length)>0]
+
+#remove duplicated values from result (as multiple transcripts might support fusion between same gene)
+#deduped.result <- unique(result)
 
 #apply function on every row in a df, but selected columns. Split the transcript list on ';' and
 #check for occurence of every split element in the quantification file. If it exists, extract the correponding fusion gene pair
@@ -35,31 +58,4 @@ apply(pizzly.fusions[,c('geneA.name','geneB.name','transcripts.list')],1, functi
     }
   }
 })
-
-#let's try using for loop for iterating over pizzly.fusions dataframe and get transcriptID and fusion gene pair information.
-#can also filter quant.sorted.filtered$target_id to have only fusion gene target ids (that is two tracscripts instead of one-
-#this will increase speed
-
-for (row in 1:nrow(pizzly.fusions)){
-  y <- strsplit(as.character(pizzly.fusions[row, 7]), "\\;")
-  y <- unname(y)
-  for (i in 1:length(y[[1]])){
-    if (y[[1]][i] %in% quant.sorted.filtered$target_id){
-      cat("row value is : " , row, "\n")
-      cat("geneA is ", as.character(pizzly.fusions[row,1]), "\n")
-      cat("gene B is ", as.character(pizzly.fusions[row, 3]), "\n")
-      val <- c(y[[1]][i], as.character(pizzly.fusions[row,1]), as.character(pizzly.fusions[row, 3]))
-      cat("val is", val, "\n")
-      result.list[[row]] <- val
-    }
-  }
-}
-
-#remove nulls from result list
-result.list <- result.list[lapply(result.list, length)>0]
-
-#remove duplicated values from result (as multiple transcripts might support fusion between same gene)
-#deduped.result <- unique(result)
-
-
 
