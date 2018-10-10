a5c270f33bb6651b40563d2ed87a337c9f10dc72
Sehrish Kanwal <kanwals@unimelb.edu.au>
2018-10-10 11:05:40 +1100

    revamping grolar script to work with fusion calls in tsv format and find genomic cordinates required for visualization of fusion genes

--- fusions/grolar-tsv.R
+++ fusions/grolar-tsv.R
@@ -1,4 +1,4 @@
-# Dapted from Matthew Bashton 2017
+# Adapted from Matthew Bashton 2017
 # Load filtered tsv output from pizzly add in gene co-ordinates + compute distance when on same chr
 
 # CRAN
@@ -6,8 +6,8 @@
 #install.packages("dplyr")
 
 # Bioconductor
-source("https://bioconductor.org/biocLite.R")
-biocLite()
+#source("https://bioconductor.org/biocLite.R")
+#biocLite()
 #biocLite("ensembldb")
 #biocLite("EnsDb.Hsapiens.v79")
 
@@ -16,11 +16,11 @@ biocLite()
 setwd("~/Documents/UMCCR/data/fusions/pizzly-validation/grolar")
 
 # CRAN libs
-#library(jsonlite)
-#library(dplyr)
+library(jsonlite)
+library(dplyr)
 
 # Assuming you have used GRCh38 gene models
-#library(EnsDb.Hsapiens.v79)
+library(EnsDb.Hsapiens.v79)
 edb <- EnsDb.Hsapiens.v79
 listColumns(edb)
 supportedFilters(edb)
@@ -33,7 +33,7 @@ suffix <- ".tsv"
 tsv_files <- list.files(path = ".", pattern = paste0("*",suffix))
 
 # Make a list of Ids
-Ids <- gsub(suffix, "", JSON_files)
+Ids <- gsub(suffix, "", tsv_files)
 
 # This function will flatten out the JSON giving you a list of gene A and gene B
 # sorted by splitcount then paircount, it will also get co-ordinates for each
@@ -44,32 +44,23 @@ GetFusionz_and_namez <- function(sample, suffix) {
   #sample <- Ids[1]
   #suffix = "_fusion_GRCh38.json"
 
-  tsv_file <- paste0(sample, .tsv)
-  cat(paste("### Working on sample:", sample, "input file:", JSON_file, "###", "\n"))
-  cat("Reading JSON..\n")
-  JSON <- fromJSON(JSON_file, flatten = TRUE)
-  cat("Extracting gene dataframe\n")
-  JSON_level1 <- JSON$genes
-  output <- JSON_level1
+  tsv_file <- paste0(sample, suffix)
 
-  # Drop weird cols
-  output <- output[,c(-3,-4)]
-
-  # Clean up IDs
-#  output$geneA.id <- gsub(".\\d+$", "", output$geneA.id, perl = TRUE)
-#  output$geneB.id <- gsub(".\\d+$", "", output$geneB.id, perl = TRUE)
+  cat(paste("### Working on sample:", sample, "input file:", tsv_file, "###", "\n"))
+  cat("Reading TSV..\n")
+  TSV <- read.table(file =  paste(getwd(), tsv_file, sep = "/"), header = TRUE)
 
   # Add uniq-key
-  output <- cbind(rownames(output), output)
+  output <- cbind(rownames(TSV), TSV)
   tmp <- colnames(output)[-1]
   colnames(output) <- c("ID", tmp)
-  #colnames(output)
+  colnames(output)
   #head(output)
 
   # Now extract geneA geneB locations
   # get all geneAs
   cat("Getting co-ordinates for gene A\n")
-  geneAs <- output$geneA.id
+  geneAs <- TSV$geneA.id
   geneAs_info <- genes(edb,
         columns = c("gene_id","seq_name","gene_seq_start","gene_seq_end","seq_strand"),
         filter = GeneIdFilter(geneAs),
@@ -77,7 +68,7 @@ GetFusionz_and_namez <- function(sample, suffix) {
 
   # get all geneBs
   cat("Getting co-ordinates for gene B\n")
-  geneBs <- output$geneB.id
+  geneBs <- TSV$geneB.id
   geneBs_info <- genes(edb,
                        columns = c("gene_id","seq_name","gene_seq_start","gene_seq_end","seq_strand"),
                        filter = GeneIdFilter(geneBs),
@@ -92,50 +83,16 @@ GetFusionz_and_namez <- function(sample, suffix) {
   # Merge in geneA_info
   cat("Merging DFs\n")
   tmp1 <- merge(output, geneAs_info, by.x = "geneA.id", by.y = "geneA.gene_id", all.x = TRUE)
-  tmp2 <- merge(output, geneBs_info, by.x = "geneB.id", by.y = "geneB.gene_id", all.x = TRUE)
+  #excluding transcripts columns, so that it is not repeated when merging tmp1 and tmp2
+  tmp2 <- merge(output[-8], geneBs_info, by.x = "geneB.id", by.y = "geneB.gene_id", all.x = TRUE)
 
   # Check we have the same length
   stopifnot(nrow(tmp1) == nrow(tmp2))
 
   # Merge these DFs
-  output <- merge(tmp1, tmp2[,c(2,8:11)], by = "ID")[,c(1,3,4,2,5,8,9,10,11,6,7,12,13,14,15)]
+  output <- merge(tmp1, tmp2[,c(2,8:11)], by = "ID")[,c(1,6,7,2,3,9,10,11,12,5,4,13,14,15,16,8)]
   colnames(output)
-
-  # Now use mutate
-  cat("Finding genes on same chr\n")
-  super_identical <- Vectorize(identical, c("x", "y"))
-  # Need to convert from DataFrame to data.frame
   output <- as.data.frame(output)
-  output <- mutate(output, same_chr = super_identical(output$geneA.seq_name,output$geneB.seq_name))
-  identical_idx <- which(output$same_chr == TRUE)
-
-
-
-  # New function for getting distance
-  GetDistance <- function(RowIdx, dat) {
-    OurRow <- dat[RowIdx,]
-    # If we have NA annotation return NA
-    if(any(is.na(c(dat[RowIdx,]$geneA.seq_name, dat[RowIdx,]$geneB.seq_name)))) {
-      return(NA)
-    }
-    # Test which starts 1st geneA or geneB
-      geneA_1st <- ifelse(OurRow$geneA.gene_seq_end < OurRow$geneB.gene_seq_start, TRUE,FALSE)
-    if(geneA_1st == TRUE) {
-      return(OurRow$geneB.gene_seq_start - OurRow$geneA.gene_seq_end)
-    } else if(geneA_1st == FALSE) {
-      return(OurRow$geneA.gene_seq_start - OurRow$geneB.gene_seq_end)
-    }
-  }
-
-  # Test
-  #GetDistance(22, output)
-  #GetDistance(476, output)
-  #GetDistance(5916, output)
-
-  # Get distances
-  cat("Computing gene distances\n")
-  geneDistance <- sapply(identical_idx, function(x) GetDistance(x, output))
-  output[identical_idx,"gene_distance"] <- geneDistance
 
   # Sort by splitcount then paircount
   cat("Sorting by number of events\n")
@@ -150,6 +107,6 @@ GetFusionz_and_namez <- function(sample, suffix) {
 }
 
 # Use above funciton on all output files
-lapply(Ids, function(x) GetFusionz_and_namez(x, suffix = ".json") )
+lapply(Ids, function(x) GetFusionz_and_namez(x, suffix = ".tsv") )
 
 
