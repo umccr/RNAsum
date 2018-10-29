2cf7b5c821a5af207912f17fe9ed094399562c04
JMarzec <jacek.d.marzec@gmail.com>
2018-10-29 18:05:18 +1100

    Add option to specify various tisssue types

--- rmd_files/RNAseq_report.R
+++ rmd_files/RNAseq_report.R
@@ -14,9 +14,10 @@
 #
 #	  Description: Script collecting user-defined parameters for the corresponding RNAseq_report.Rmd markdown script generating the "UMCCR Transcriptome Patient Summary" report. Note, only genes intersection between the sample read count file and the reference datasets expression matrices will be considered in the analyses.
 #
-#	  Command line use example: Rscript RNAseq_report.R  --sample_name CCR170012_MH17T001P013  --count_file ../data/CCR170012_MH17T001P013-ready.counts  --report_dir ../reports
+#	  Command line use example: Rscript RNAseq_report.R  --sample_name CCR170012_MH17T001P013  --tissue pancreas  --count_file ../data/CCR170012_MH17T001P013-ready.counts  --report_dir ../reports
 #
 #   sample_name:   Desired sample name to be presented in the report
+#   tissue:        Tissue from which the samples were derived
 #   count_file:    Location and name of the read count file from bcbio RNA-seq pipeline
 #   report_dir:    Desired location for the report
 #
@@ -33,6 +34,12 @@ graphics.off()
 
 suppressMessages(library(optparse))
 
+#===============================================================================
+#    Define functions
+#===============================================================================
+
+##### Create 'not in' operator
+"%!in%" <- function(x,table) match(x,table, nomatch = 0) == 0
 
 #===============================================================================
 #    Catching the arguments
@@ -41,6 +48,8 @@ suppressMessages(library(optparse))
 option_list = list(
   make_option(c("-s", "--sample_name"), action="store", default=NA, type='character',
               help="Desired sample name to be presented in the report"),
+  make_option(c("-t", "--tissue"), action="store", default=NA, type='character',
+              help="Tissue from which the samples were derived"),
   make_option(c("-c", "--count_file"), action="store", default=NA, type='character',
               help="Location and name of the read count file from bcbio RNA-seq pipeline"),
   make_option(c("-r", "--report_dir"), action="store", default=NA, type='character',
@@ -50,14 +59,23 @@ option_list = list(
 opt = parse_args(OptionParser(option_list=option_list))
 
 ##### Read in argument from command line and check if all were provide by the user
-if ( is.na(opt$sample_name) || is.na(opt$count_file) || is.na(opt$report_dir) ) {
+if ( is.na(opt$sample_name) || is.na(opt$tissue) || is.na(opt$count_file) || is.na(opt$report_dir) ) {
 
   cat("\nPlease type in required arguments!\n\n")
-  cat("\ncommand example:\n\nRscript RNAseq_report.R  --sample_name CCR170012_MH17T001P013  --count_file ../data/CCR170012_MH17T001P013-ready.counts  --report_dir ../reports\n\n")
+  cat("\ncommand example:\n\nRscript RNAseq_report.R  --sample_name CCR170012_MH17T001P013  --tissue pancreas  --count_file ../data/CCR170012_MH17T001P013-ready.counts  --report_dir ../reports\n\n")
 
   q()
 }
 
+##### Check if specified tissue type is valid
+if ( opt$tissue %!in% c("pancreas", "cervix") ) {
+  
+  cat("\nInvalid tissue type! Please use one of the following:\n\n")
+  cat("[ pancreas ] - this will compare the patient's data in the context of samples from TCGA pancreatic ductal adenocarcinoma (PAAD) cohort and GTEx samples from healthy pancreas tissue\n")
+  cat("[ cervix ] - this will compare the patient's data in the context of samples from TCGA cervical squamous cell carcinoma (CESC) cohort and GTEx samples from healthys cervix uteri tissue\n\n")
+  
+  q()
+}
 
 ##### Pass the user-defined arguments to the RNAseq_report R markdown script and generate the report
-rmarkdown::render(input = "RNAseq_report.Rmd", output_file = paste0(opt$sample_name, ".RNAseq_report.html"), output_dir = opt$report_dir, params = list(report_dir = opt$report_dir, sample_name = opt$sample_name, count_file = opt$count_file))
+rmarkdown::render(input = "RNAseq_report.Rmd", output_file = paste0(opt$sample_name, ".RNAseq_report.html"), output_dir = opt$report_dir, params = list(report_dir = opt$report_dir, sample_name = opt$sample_name, tissue = opt$tissue, count_file = opt$count_file))
