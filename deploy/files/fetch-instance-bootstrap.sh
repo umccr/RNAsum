e82efb6f20ae19b0a064002421d96fbe63c25b55
Sehrish Kanwal <kanwals@unimelb.edu.au>
2019-09-02 09:32:39 +1000

    remover docker start-up from instance bootstrap script

--- deploy/files/fetch-instance-bootstrap.sh
+++ deploy/files/fetch-instance-bootstrap.sh
@@ -6,11 +6,3 @@ SCRIPT="bootstrap-instance.sh"
 
 # Fetch the bootstrap script 
 wget https://raw.githubusercontent.com/umccr/workflows/master/$STACK/$SCRIPT -O "/$SCRIPT" && chmod +x "/$SCRIPT"
-
-# Install and start docker service
-sudo yum update -y
-sudo amazon-linux-extras install docker
-sudo service docker start
-
-# Add the ssm-user to the docker group 
-sudo usermod -a -G docker ssm-user
