9f37641cf174fdb4fcfbdcd077c94ed23f46f149
Sehrish Kanwal <kanwals@unimelb.edu.au>
2019-09-02 11:12:16 +1000

    move docker setup

--- deploy/files/fetch-instance-bootstrap.sh
+++ deploy/files/fetch-instance-bootstrap.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
 
+# Install and start docker service
+sudo yum update -y
+sudo amazon-linux-extras install docker -y
+sudo service docker start
+
+# Add the ssm-user to the docker group 
+sudo usermod -a -G docker ssm-user
+
 STACK="rna-seq_report"
 SCRIPT="bootstrap-instance.sh"
 
