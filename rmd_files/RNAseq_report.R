2184c78449ebc79ae899c6064f69b85c9d772928
JMarzec <jacek.d.marzec@gmail.com>
2019-09-16 11:55:05 +1000

    Update arguments

--- rmd_files/RNAseq_report.R
+++ rmd_files/RNAseq_report.R
@@ -34,7 +34,7 @@
 #   cn_gain (optional):  CN threshold value to classify genes within gained regions (default is "95th percentile" of all CN values)
 #   clinical_info (optional): Location of xslx file with clinical information
 #   subject_id (optional):    Subject ID required to match sample with clinical information (specified in flag --clinical_info)
-#   sample_type (optional):   Type of investigated sample (e.g. fresh frozen tissue, organoid). This information is for annotation purposes only
+#   sample_source (optional):   Source of investigated sample (e.g. fresh frozen tissue, organoid). This information is for annotation purposes only
 #   project (optional):   Project name. This information is for annotation purposes only
 #   top_genes:    The number of top ranked genes to be presented (default is "10")
 #   dataset_name_incl:  Include dataset in the report name. Available options are: "TRUE" and "FALSE" (default)
@@ -105,7 +105,7 @@ option_list = list(
               help="Location of xslx file with clinical information"),
   make_option(c("-si", "--subject_id"), action="store", default=NA, type='character',
               help="Subject ID"),
-  make_option(c("-st", "--sample_type"), action="store", default="-", type='character',
+  make_option(c("-st", "--sample_source"), action="store", default="-", type='character',
               help="Type of investigated sample"),
   make_option(c("-pr", "--project"), action="store", default="-", type='character',
               help="Project name"),
@@ -249,7 +249,7 @@ if ( !file.exists(opt$report_dir) ) {
 }
 
 ##### Pass the user-defined arguments to the RNAseq_report R markdown script and generate the report
-rmarkdown::render(input = "RNAseq_report.Rmd", output_file = paste0(opt$sample_name, toupper(dataset_name_incl), ".RNAseq_report.html"), output_dir = opt$report_dir, params = list(sample_name = opt$sample_name, dataset = toupper(opt$dataset), bcbio_rnaseq = opt$bcbio_rnaseq, report_dir = opt$report_dir, ref_data_dir = opt$ref_data_dir, transform = opt$transform, norm = opt$norm, batch_rm = opt$batch_rm, filter = opt$filter, log = opt$log, scaling = opt$scaling, immunogram = opt$immunogram, umccrise = opt$umccrise, clinical_info = opt$clinical_info, subject_id = opt$subject_id, sample_type = opt$sample_type, project = opt$project, dataset_name_incl = dataset_name_incl, save_tables = opt$save_tables, pcgr_tier = opt$pcgr_tier, pcgr_splice_vars = opt$pcgr_splice_vars, cn_loss = opt$cn_loss, cn_gain = opt$cn_gain, top_genes = opt$top_genes, hide_code_btn = opt$hide_code_btn, grch_version = as.numeric(opt$grch_version), ensembl_version = as.numeric(ensembl_version), ucsc_genome_assembly = as.numeric(ucsc_genome_assembly)))
+rmarkdown::render(input = "RNAseq_report.Rmd", output_file = paste0(opt$sample_name, toupper(dataset_name_incl), ".RNAseq_report.html"), output_dir = opt$report_dir, params = list(sample_name = opt$sample_name, dataset = toupper(opt$dataset), bcbio_rnaseq = opt$bcbio_rnaseq, report_dir = opt$report_dir, ref_data_dir = opt$ref_data_dir, transform = opt$transform, norm = opt$norm, batch_rm = opt$batch_rm, filter = opt$filter, log = opt$log, scaling = opt$scaling, immunogram = opt$immunogram, umccrise = opt$umccrise, clinical_info = opt$clinical_info, subject_id = opt$subject_id, sample_source = opt$sample_source, project = opt$project, dataset_name_incl = dataset_name_incl, save_tables = opt$save_tables, pcgr_tier = opt$pcgr_tier, pcgr_splice_vars = opt$pcgr_splice_vars, cn_loss = opt$cn_loss, cn_gain = opt$cn_gain, top_genes = opt$top_genes, hide_code_btn = opt$hide_code_btn, grch_version = as.numeric(opt$grch_version), ensembl_version = as.numeric(ensembl_version), ucsc_genome_assembly = as.numeric(ucsc_genome_assembly)))
 
 ##### Remove the assocaited MD file and the redundant folder with plots that are imbedded in the HTML report
 unlink(paste0(opt$report_dir, "/", opt$sample_name, toupper(dataset_name_incl), ".RNAseq_report.md"), recursive = TRUE)
