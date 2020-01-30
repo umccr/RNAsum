976554594b0fe6161e1c8d46175d9279245553f1
JMarzec <jacek.d.marzec@gmail.com>
2020-01-30 17:24:19 +1100

    Add ref dataset and save tables params test run wrapper

--- envm/wts-report-wrapper.sh
+++ envm/wts-report-wrapper.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 echo "Run WTS-report"
-Rscript /rmd_files/RNAseq_report.R --sample_name test_sample_WTS  --bcbio_rnaseq /work/data/test_data/final/test_sample_WTS --report_dir /work/output --umccrise /work/data/test_data/umccrised/test_subject__test_sample_WGS --ref_data_dir /work/data
+Rscript /rmd_files/RNAseq_report.R --sample_name test_sample_WTS  --bcbio_rnaseq /work/data/test_data/final/test_sample_WTS --report_dir /work/output --umccrise /work/data/test_data/umccrised/test_subject__test_sample_WGS --ref_data_dir /work/data --dataset TEST --save_tables FALSE
