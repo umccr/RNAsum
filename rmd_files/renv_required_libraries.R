f53fb739559daccaba8d318c494236d0ad268b41
Alexis Lucattini <alexis.lucattini@umccr.org>
2022-03-28 10:28:04 +1100

    Installed renv into dockerfile

---
+++ rmd_files/renv_required_libraries.R
@@ -0,0 +1,7 @@
+#!/usr/bin/env Rscript
+
+# Dependencies that must be explicity in the project in 
+# order for renv to download them
+suppressMessages(library(MASS))
+suppressMessages(library(mgcv))
+suppressMessages(library(rpart))
