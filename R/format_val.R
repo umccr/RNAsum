96d2c19ee6a80fcd90e32ff88cad1a0a5dd7d26d
Sehrish <kanwals@unimelb.edu.au>
2022-05-27 12:00:07 +1000

    move this function to sv_prioritize.R

--- R/format_val.R
+++
@@ -1,5 +0,0 @@
-##### Function used in the "sv_prioritize" function
-format_val = function(val, is_pct = F) {
-  ifelse(!is.na(val),
-         format(val,  digits = 1) %>% str_c(ifelse(is_pct, "%", "")), NA)
-}
