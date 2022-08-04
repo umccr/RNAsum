4352ae5f5560c625d8f8a762d8d10da0b1a7118b
pdiakumis <peterdiakumis@gmail.com>
2022-08-05 00:36:35 +1000

    add get_refgenes convenience func

--- R/refdata.R
+++ R/refdata.R
@@ -26,3 +26,48 @@ get_refdata <- function(dataset) {
     )
   )
 }
+
+#' Get Reference Genes
+#'
+#' Get a list of reference genes.
+#' @param p RNAsum params list.
+#' @return List of tibbles corresponding to cancer, oncokb, immune and hrd genes
+#'
+#' @examples
+#' p <- list(
+#'   genes_cancer = system.file("rawdata/genes/umccr_cancer_genes.2019-03-20.tsv", package = "RNAsum"),
+#'   genes_oncokb = system.file("rawdata/OncoKB/CancerGenesList.txt", package = "RNAsum"),
+#'   genes_immune_markers = system.file("rawdata/genes/Genes_immune_markers.txt", package = "RNAsum"),
+#'   genes_hrd = system.file("rawdata/genes/Genes_HRD.txt", package = "RNAsum")
+#' )
+#' x <- get_refgenes(p)
+#' @testexamples
+#' expect_equal(length(x), 4)
+#' expect_null(x$foo)
+#' @export
+get_refgenes <- function(p) {
+  .read <- function(x, ...) {
+    if (is.null(x)) {
+      return(NULL)
+    }
+    x |>
+      readr::read_tsv(col_types = readr::cols(...))
+  }
+  genes_cancer <- p[["genes_cancer"]] |>
+    .read(.default = "l", driver = "d", n = "i", symbol = "c", sources = "c")
+  genes_oncokb <- p[["genes_oncokb"]] |>
+    .read(.default = "c", "# of occurence within resources" = "i")
+  genes_hrd <- .read(p[["genes_hrd"]], SYMBOL = "c")
+  genes_immune_markers <- .read(p[["genes_immune_markers"]], .default = "c")
+  genes_immunogram <- .read(p[["genes_immunogram"]], .default = "c")
+
+  list(
+    genes_cancer = genes_cancer,
+    genes_oncokb = genes_oncokb,
+    genes_hrd = genes_hrd,
+    genes_immune = list(
+      genes_immune_markers = genes_immune_markers,
+      genes_immunogram = genes_immunogram
+    )
+  )
+}
