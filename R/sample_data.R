6a41b70932deeb8db204756cfe3ddf1e334069c2
pdiakumis <peterdiakumis@gmail.com>
2022-08-05 00:38:29 +1000

    add clinical_info_read convenience func

--- R/sample_data.R
+++ R/sample_data.R
@@ -1,7 +1,7 @@
 #' Read Sample Data
 #'
-#' Reads sample data, including Arriba fusions, Arriba plots, Salmon counts, and
-#' DRAGEN fusions.
+#' Reads sample data, including Arriba fusions, Arriba plots, Salmon counts,
+#' DRAGEN fusions, and clinical info.
 #'
 #' @param p RNAsum params list.
 #' @param results_dir Directory to output extracted Arriba PNGs to (created
@@ -15,7 +15,6 @@
 #'   dragen_rnaseq = system.file("rawdata/test_data/dragen", package = "RNAsum"),
 #'   arriba_pdf = system.file("rawdata/test_data/dragen/arriba/fusions.pdf", package = "RNAsum"),
 #'   arriba_tsv = system.file("rawdata/test_data/dragen/arriba/fusions.tsv", package = "RNAsum"),
-#'   salmon = NULL,
 #'   dragen_fusions = system.file(
 #'     "rawdata/test_data/dragen/test_sample_WTS.fusion_candidates.final",
 #'     package = "RNAsum"
@@ -23,25 +22,21 @@
 #' )
 #' res <- read_sample_data(p, tempdir())
 #' @testexamples
-#' expect_equal(length(res), 4)
+#' expect_equal(length(res), 5)
 #' expect_null(res$salmon)
 #' @export
 read_sample_data <- function(p, results_dir, tx2gene = NULL) {
-  assertthat::assert_that(
-    all(c("arriba_pdf", "arriba_tsv", "salmon", "dragen_fusions") %in% names(p))
-  )
-
   arriba_tsv <- arriba_read_tsv(p[["arriba_tsv"]])
-  arriba_pdf <- arriba_read_pdf(p[["arriba_pdf"]],
-    fusions = arriba_tsv,
-    outdir = file.path(results_dir, "arriba")
-  )
+  arriba_pdf <- p[["arriba_pdf"]] |>
+    arriba_read_pdf(fusions = arriba_tsv, outdir = file.path(results_dir, "arriba"))
   salmon <- salmon_counts(p[["salmon"]], tx2gene = tx2gene)
   dragen_fusions <- dragen_fusions_read(p[["dragen_fusions"]])
+  clininfo <- clinical_info_read(p[["clinical_info"]])
   list(
     arriba_tsv = arriba_tsv,
     arriba_pdf = arriba_pdf,
     salmon = salmon,
-    dragen_fusions = dragen_fusions
+    dragen_fusions = dragen_fusions,
+    clinical_info = clininfo
   )
 }
