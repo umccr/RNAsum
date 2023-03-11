940db3d0518be3cec16a388642f21a9aa30829a6
pdiakumis <peterdiakumis@gmail.com>
2023-03-12 00:28:08 +1100

    refactor library_size_plot chunk

--- R/saveWidgetFix.R
+++ R/saveWidgetFix.R
@@ -1,19 +1,28 @@
 #' Wrapper for saveWidget bug fix
 #'
-#' A wrapper to saveWidget which compensates for arguable BUG in saveWidget which requires
-#' `file` to be in current working directory (see https://github.com/ramnathv/htmlwidgets/issues/299).
+#' A wrapper to saveWidget which compensates for bug where the temporary
+#' directory with widget dependencies is not deleted - see
+#' https://github.com/ramnathv/htmlwidgets/issues/296.
 #'
-#' @param widget Input widget.
-#' @param file Input file.
+#' @param widget Widget to save.
+#' @param file Full path to save HTML into.
+#' @param selfcontained Whether to save the HTML as a single self-contained file
+#' (with external resources base64 encoded) or a file with external resources
+#' placed in an adjacent directory.
 #' @param ... Additional arguments passed to [htmlwidgets::saveWidget].
 #'
 #' @return Change work dir to compensate for saveWidget bug
 #' @export
-saveWidgetFix <- function ( widget, file, ...) {
-  wd<-getwd()
-  on.exit(setwd(wd))
-  outDir<-dirname(file)
-  file<-basename(file)
-  setwd(outDir);
-  htmlwidgets::saveWidget(widget,file=file,...)
+saveWidgetFix <- function(widget, file, selfcontained = TRUE, ...) {
+  assertthat::assert_that(
+    base::inherits(widget, "htmlwidget")
+  )
+  file_dir <- base::dirname(file)
+  mkdir(file_dir)
+  file_bname <- base::basename(file)
+  file_bname_noext <- tools::file_path_sans_ext(file_bname)
+  dir2del <- file.path(file_dir, glue::glue("{file_bname_noext}_files"))
+  # assertthat::assert_that(dir.exists(dir2del))
+  htmlwidgets::saveWidget(widget = widget, file = file, selfcontained = selfcontained, ...)
+  base::unlink(dir2del, recursive = TRUE)
 }
