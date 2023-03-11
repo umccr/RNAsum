2875e83eef11f11eb3b9a6156fdf187dc97aeacd
pdiakumis <peterdiakumis@gmail.com>
2023-03-11 18:23:40 +1100

    use safer seq_len

--- R/getColours.R
+++ R/getColours.R
@@ -7,21 +7,21 @@
 #' @return Colour assignment to different elements
 #' @export
 getColours <- function(elements) {
-
   ##### Predefined selection of colours for elements
-  if ( length(unique(elements)) == 3 ) {
+  if (length(unique(elements)) == 3) {
     elements.colours <- c("powderblue", "red", "gray50")
-  } else if ( length(unique(elements)) == 4 ) {
+  } else if (length(unique(elements)) == 4) {
     elements.colours <- c("powderblue", "forestgreen", "red", "gray50")
   } else {
     elements.colours <- grDevices::rainbow(length(elements))
   }
 
   f.elements <- factor(elements, levels = unique(elements))
-  vec.elements <- elements.colours[1:length(levels(f.elements))]
-  elements.colour <- rep(0,length(f.elements))
-  for (i in 1:length(f.elements))
-    elements.colour[i] <- vec.elements[ f.elements[i]==levels(f.elements)]
+  vec.elements <- elements.colours[seq_len(length(levels(f.elements)))]
+  elements.colour <- rep(0, length(f.elements))
+  for (i in seq_len(length(f.elements))) {
+    elements.colour[i] <- vec.elements[f.elements[i] == levels(f.elements)]
+  }
 
-  return( list(vec.elements, elements.colour) )
+  list(vec.elements, elements.colour)
 }
