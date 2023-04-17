777c8492b923758ee5b9b1eb29695a0aef1a7759
pdiakumis <peterdiakumis@gmail.com>
2023-04-17 13:32:16 +1000

    add empty_tbl util function

--- R/utils.R
+++ R/utils.R
@@ -64,3 +64,15 @@ is.empty <- function(x, trim = TRUE, ...) {
     return(sapply(x, is.empty, trim = trim, ...))
   }
 }
+
+#' Create Empty Tibble Given Column Names
+#'
+#' From https://stackoverflow.com/a/62535671/2169986. Useful for handling
+#' edge cases with empty data.
+#'
+#' @param cnames Character vector of column names to use.
+#' @return A tibble with 0 rows and the given column names.
+#' @export
+empty_tbl <- function(cnames, ctypes = readr::cols(.default = "c")) {
+  read_csv("\n", col_names = cnames, col_types = ctypes)
+}
