2cfa9e79c1869a7ba33fe3164c413860d89d880a
Sehrish <kanwals@unimelb.edu.au>
2023-07-05 15:34:36 +1000

    save complete gene and tx ids to a file

--- data-raw/ah_edb.R
+++ data-raw/ah_edb.R
@@ -1,9 +1,15 @@
-# This script is used to download a specific ensembl database version using Annotation Hub.
+# This script is used to download a specific ensembl database version using Annotation Hub and extract tarnscript and gene IDS for a specific version
 require(AnnotationHub)
 require(here)
 
 ah <- AnnotationHub()
 # Downloading v105
 edb_105 <- ah[["AH98047", force=TRUE]]
-# Save RDS object to a file
-saveRDS(edb_105, file = here::here("inst/extdata/edb_105.rds"))
+# Find keys
+keys_ah <- AnnotationDbi::keys(edb_105, keytype = "GENEID")
+# Extract gene and transcript ids
+tx_gene_id <- edb_105 |>
+  AnnotationDbi::select(keys = keys_ah, columns = c("TXID", "GENEID"), keytype = "GENEID") |>
+  dplyr::rename(tx_name = "TXID", gene_id = "GENEID")
+# Save gene and transcript ids to a RDS file
+saveRDS(tx_gene_id, file = here::here("inst/extdata/tx_gene_id_105.rds"))
