0b0e2ea60216fb3211e355c58f5e5af392fb7364
Sehrish <kanwals@unimelb.edu.au>
2023-07-07 14:40:13 +1000

    Revert "save complete gene and tx ids to a file"
    This reverts commit 2cfa9e79c1869a7ba33fe3164c413860d89d880a.

--- data-raw/ah_edb.R
+++ data-raw/ah_edb.R
@@ -1,15 +1,9 @@
-# This script is used to download a specific ensembl database version using Annotation Hub and extract tarnscript and gene IDS for a specific version
+# This script is used to download a specific ensembl database version using Annotation Hub.
 require(AnnotationHub)
 require(here)
 
 ah <- AnnotationHub()
 # Downloading v105
 edb_105 <- ah[["AH98047", force=TRUE]]
-# Find keys
-keys_ah <- AnnotationDbi::keys(edb_105, keytype = "GENEID")
-# Extract gene and transcript ids
-tx_gene_id <- edb_105 |>
-  AnnotationDbi::select(keys = keys_ah, columns = c("TXID", "GENEID"), keytype = "GENEID") |>
-  dplyr::rename(tx_name = "TXID", gene_id = "GENEID")
-# Save gene and transcript ids to a RDS file
-saveRDS(tx_gene_id, file = here::here("inst/extdata/tx_gene_id_105.rds"))
+# Save RDS object to a file
+saveRDS(edb_105, file = here::here("inst/extdata/edb_105.rds"))
