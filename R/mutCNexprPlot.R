97f454d6d96d37f27d518f75d3240977449db816
pdiakumis <peterdiakumis@gmail.com>
2023-07-08 13:39:59 +1000

    mutCNexprPlot refactor

--- R/mutCNexprPlot.R
+++ R/mutCNexprPlot.R
@@ -10,77 +10,82 @@
 #' @param type Type.
 #' @param report_dir Report directory.
 #'
-#' @importFrom magrittr %>%
 #' @return Scatterplot with per-gene expression values (y-axis), CN values (x-axis) and mutation status info (colours), if provided.
 #' @export
 mutCNexprPlot <- function(data, alt_data = FALSE, cn_bottom = cn_bottom, cn_top = cn_top, comp_cancer, type = "z", report_dir) {
-
   ##### Extract info for genes to be annotated on the plot
-  genes2annot <- data[ data$CN >= cn_top | data$CN <= cn_bottom ,]$Gene
+  genes2annot <- data |>
+    dplyr::filter(.data$CN >= cn_top | .data$CN <= cn_bottom) |>
+    dplyr::pull(.data$Gene)
 
-  if ( length(genes2annot) == 0 ) {
+  if (length(genes2annot) == 0) {
     genes2annot <- ""
   }
 
-  if ( type == "z" ) {
-    names(data)[ names(data) %in% "Z_score_diff" ] <- "Expr"
+  if (type == "z") {
+    names(data)[names(data) %in% "Diff_Z_score"] <- "Expr"
     y_title <- paste0("mRNA expression (Z-score [Patient vs ", comp_cancer, "])")
-
-  } else if ( type == "perc" ) {
-    names(data)[ names(data) %in% "Perc_diff" ] <- "Expr"
+  } else if (type == "perc") {
+    names(data)[names(data) %in% "Diff_Perc"] <- "Expr"
     y_title <- paste0("mRNA expression (percentile [Patient vs ", comp_cancer, "])")
   }
 
   ##### Generate scatterplot with per-gene expression values (y-axis) (difference between Patient's and [comp_cancer] data), CN values (x-axis) and mutation status info (colours)
-  if ( alt_data ) {
-    p <- plotly::plot_ly(type='scatter', mode = "markers", width = 800, height = 600, showlegend = FALSE) %>%
-
-      plotly::add_markers(data = data, y = ~Expr, x = ~CN,
-                  name = ~Gene,
-                  text = paste0("Gene: ", data$Gene,  "\nAlterations: ", data$Alterations),
-                  mode = 'markers',
-                  marker = list(size=10, symbol="circle"),
-                  color = ~Gene,
-                  showlegend = TRUE,
-                  legendtitle=TRUE,
-                  inherit = FALSE) %>%
-
-      plotly::add_annotations( data = data[ data$CN >= cn_top | data$CN <= cn_bottom ,], text=genes2annot,
-                       x=~CN, xanchor="left",
-                       y=~Expr, yanchor="top",
-                       font = list(color = "Grey", size = 10),
-                       legendtitle=TRUE, showarrow=FALSE ) %>%
-
-      plotly::layout( xaxis = list(title = "CN value"), yaxis = list(title = y_title), margin = list(l=50, r=50, b=50, t=50, pad=4), autosize = F, legend = list( orientation = 'v', x=1, y=0.97, yanchor="top"), showlegend=TRUE)
-
+  if (alt_data) {
+    p <- plotly::plot_ly(
+      type = "scatter", mode = "markers", width = 800, height = 600, showlegend = FALSE
+    ) |>
+      plotly::add_markers(
+        data = data, y = ~Expr, x = ~CN,
+        name = ~Gene,
+        text = paste0("Gene: ", data$Gene, "\nAlterations: ", data$Alterations),
+        mode = "markers",
+        marker = list(size = 10, symbol = "circle"),
+        color = ~Gene,
+        showlegend = TRUE,
+        # legendtitle=TRUE,
+        inherit = FALSE
+      ) |>
+      plotly::add_annotations(
+        data = data |> dplyr::filter(.data$CN >= cn_top | .data$CN <= cn_bottom),
+        text = genes2annot,
+        x = ~CN, xanchor = "left",
+        y = ~Expr, yanchor = "top",
+        font = list(color = "Grey", size = 10),
+        legendtitle = TRUE, showarrow = FALSE
+      ) |>
+      plotly::layout(
+        xaxis = list(title = "CN value"), yaxis = list(title = y_title),
+        margin = list(l = 50, r = 50, b = 50, t = 50, pad = 4),
+        autosize = FALSE, legend = list(orientation = "v", x = 1, y = 0.97, yanchor = "top"),
+        showlegend = TRUE
+      )
     ##### Generate scatterplot with per-gene expression values (y-axis) and CN values (x-axis)
   } else {
-    p <- plotly::plot_ly(data, x = ~CN, y = ~Expr, text=~Gene, color = ~Gene, type='scatter', mode = "markers", marker = list(size=10, symbol="circle"), width = 800, height = 600) %>%
-
-      plotly::add_annotations( data = data[ data$CN >= cn_top | data$CN <= cn_bottom ,], text=~Gene,
-                       x=~CN, xanchor="left",
-                       y=~Expr, yanchor="top",
-                       font = list(color = "Grey",
-                                   size = 10),
-                       legendtitle=TRUE, showarrow=FALSE ) %>%
-
-      plotly::layout( xaxis = list(title = "CN value"), yaxis = list(title =  y_title), margin = list(l=50, r=50, b=50, t=50, pad=4), autosize = F, legend = list( orientation = 'v', y=0.8, yanchor="top"), showlegend=TRUE)
+    p <- plotly::plot_ly(
+      data,
+      x = ~CN, y = ~Expr, text = ~Gene, color = ~Gene, type = "scatter",
+      mode = "markers", marker = list(size = 10, symbol = "circle"),
+      width = 800, height = 600
+    ) |>
+      plotly::add_annotations(
+        data = data |> dplyr::filter(.data$CN >= cn_top | .data$CN <= cn_bottom),
+        text = ~Gene, x = ~CN, xanchor = "left", y = ~Expr, yanchor = "top",
+        font = list(color = "Grey", size = 10),
+        legendtitle = TRUE, showarrow = FALSE
+      ) |>
+      plotly::layout(
+        xaxis = list(title = "CN value"), yaxis = list(title = y_title),
+        margin = list(l = 50, r = 50, b = 50, t = 50, pad = 4), autosize = FALSE,
+        legend = list(orientation = "v", y = 0.8, yanchor = "top"), showlegend = TRUE
+      )
   }
 
   ##### Create directory for the plots
-  mutCNexprPlotDir <- paste(report_dir, "cn_expr_plot", sep = "/")
-  if ( !file.exists(mutCNexprPlotDir) ) {
-    dir.create(mutCNexprPlotDir, recursive=TRUE)
-  }
+  mutCNexprPlotDir <- file.path(report_dir, "cn_expr_plot")
+  fs::dir_create(mutCNexprPlotDir)
 
   ##### Save interactive plot as html file
-  saveWidgetFix(p, file = paste(mutCNexprPlotDir, paste0("cn_expr_plot.", type, ".html"), sep = "/"))
-
-  return( p )
-
-  ##### Clean the space and return output
-  rm(data, alt_data, genes2annot, y_title)
-
-  #### Clear plots to free up some memory
-  if(!is.null(grDevices::dev.list())) invisible(grDevices::dev.off())
+  RNAsum::saveWidgetFix(widget = p, file = file.path(mutCNexprPlotDir, paste0("cn_expr_plot.", type, ".html")))
+  return(p)
 }
