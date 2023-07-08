e7c900a467fcac5f9ce5d0db6671bb9f80c17ab2
pdiakumis <peterdiakumis@gmail.com>
2023-07-08 21:50:56 +1000

    addendum refactor

--- R/utils.R
+++ R/utils.R
@@ -11,3 +11,53 @@
 empty_tbl <- function(cnames, ctypes = readr::cols(.default = "c")) {
   readr::read_csv("\n", col_names = cnames, col_types = ctypes)
 }
+
+#' Session Information Kable
+#'
+#' Session information kables for vignettes.
+#'
+#' @param pkgs Vector of R packages to display in the vignette. By default returns all.
+#'
+#' @return A list with two kables containing information about the platform and
+#' the specified packages.
+#' @export
+session_info_kable <- function(pkgs = NULL) {
+  si <- session_info_tbls(pkgs)
+  si_pl <- si$si_pl
+  si_pkg <- si$si_pkg
+  list(
+    si_pl = knitr::kable(si_pl, caption = "Platform information."),
+    si_pkg = knitr::kable(si_pkg, caption = "Main packages used.")
+  )
+}
+
+#' Session Information Tables
+#'
+#' Session information tables.
+#'
+#' @param pkgs Vector of R packages to display in the vignette. By default returns all.
+#'
+#' @return A list with two tibbles containing information about the platform and
+#' the specified packages.
+#' @export
+session_info_tbls <- function(pkgs = NULL) {
+  si <- devtools::session_info(include_base = TRUE)
+  assertthat::assert_that(all(c("platform", "packages") %in% names(si)))
+  si_pl <- unclass(si[["platform"]]) |>
+    unlist() |>
+    tibble::enframe(name = "name", value = "value")
+  si_pkg <- unclass(si[["packages"]]) |>
+    dplyr::as_tibble() |>
+    dplyr::select(
+      "package",
+      version = "ondiskversion", datestamp = "date", "source"
+    )
+  if (!is.null(pkgs)) {
+    si_pkg <- si_pkg |>
+      dplyr::filter(.data$package %in% pkgs)
+  }
+  list(
+    si_pl = si_pl,
+    si_pkg = si_pkg
+  )
+}
