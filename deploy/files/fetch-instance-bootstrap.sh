846c5a45221a017e82c9c679bb34bcfe5e6592f1
pdiakumis <peterdiakumis@gmail.com>
2023-07-10 23:50:49 +1000

    deploy: reorg/delete scripts

--- deploy/files/fetch-instance-bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-STACK="rna-seq_report"
-SCRIPT="bootstrap-instance.sh"
-
-# Fetch the bootstrap script 
-wget https://raw.githubusercontent.com/umccr/workflows/master/$STACK/$SCRIPT -O "/$SCRIPT" && chmod +x "/$SCRIPT" && /bin/bash "/$SCRIPT"
