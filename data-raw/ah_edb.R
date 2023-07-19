1513c1298ad2c22cc9dece5bb70cd420b75fe01f
Sehrish <kanwals@unimelb.edu.au>
2023-07-19 17:13:08 +1000

    use saved rds file to get info from ensembl db

--- data-raw/ah_edb.R
+++ data-raw/ah_edb.R
@@ -9,7 +9,10 @@ edb_105 <- ah[["AH98047", force=TRUE]]
 keys_ah <- AnnotationDbi::keys(edb_105, keytype = "GENEID")
 # Extract gene and transcript ids
 tx_gene_id <- edb_105 |>
-  AnnotationDbi::select(keys = keys_ah, columns = c("TXID", "GENEID"), keytype = "GENEID") |>
-  dplyr::rename(tx_name = "TXID", gene_id = "GENEID")
+  AnnotationDbi::select(
+    keys = keys_ah,
+    columns = c(
+      "TXID", "GENEID", "GENENAME", "GENEBIOTYPE", "SEQNAME", "GENESEQSTART", "GENESEQEND"
+      ), keytype = "GENEID")
 # Save gene and transcript ids to a RDS file
 saveRDS(tx_gene_id, file = here::here("inst/extdata/tx_gene_id_105.rds"))
