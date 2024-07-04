6dc4805110b98abd9d8f7ab6a0e00eda839b88c7
pdiakumis <peterdiakumis@gmail.com>
2024-07-04 21:58:19 +1000

    pkgs no longer required; fix devtools::check

--- R/utils_shortcuts.R
+++ R/utils_shortcuts.R
@@ -9,3 +9,18 @@ NULL
 
 #' @keywords internal
 "_PACKAGE"
+
+#' @noRd
+dummy1 <- function() {
+  # Solves R CMD check: Namespaces in Imports field not imported from
+  EDASeq::plotRLE
+  conflicted::conflict_scout
+  edgeR::DGEList
+  ggforce::geom_sina
+  ggplot2::ggplot
+  limma::removeBatchEffect
+  manhattanly::manhattanly
+  optparse::make_option
+  preprocessCore::normalize.quantiles
+  ragg::agg_png
+}
